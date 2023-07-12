65c16436a95d7100a8e145f6e2e69521aaddc8aa
Malhar Choure <chouremalhar@gmail.com>
2023-07-12 16:58:51 +0530

    Camera Swivel and Input Test System Added
    This commit contains the Camera Swivel system and the test system.

--- Assets/Scripts/PicaVoxelEditor/CameraOperationScripts/CamPerspectiveScript.cs
+++ Assets/Scripts/PicaVoxelEditor/CameraOperationScripts/CamPerspectiveScript.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class CamPerspectiveScript : MonoBehaviour
 {
+    /// <summary>
+    /// Main cam will basically have the camera component to allow it to change projection
+    /// </summary>
     [SerializeField]
     private Camera _MainCam;
+    /// <summary>
+    /// _IsOrthographic checks if the projection is orthographic or not
+    /// </summary>
     private bool _IsOrthographic = false;
+    /// <summary>
+    /// _IsPerspective checks if the projection is perspective or not
+    /// </summary>
     private bool _IsPerspective = true;
 
+
+    /// <summary>
+    /// This method swaps projection to Perspective
+    /// </summary>
     public void SwapToPerspective()
     {
         if(!_IsPerspective && _IsOrthographic)
@@ -19,6 +32,10 @@ public class CamPerspectiveScript : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// This method swaps projection to Orthographic
+    /// </summary>
     public void SwapToOrthographic()
     {
         if (_IsPerspective && !_IsOrthographic)
