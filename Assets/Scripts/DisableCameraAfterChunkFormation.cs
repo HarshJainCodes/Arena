381a96ff1968bbd2d42b73d68d77eeadb41a1de6
Harsh Jain <harshjain17may@gmail.com>
2023-07-12 20:39:20 +0530

    fixed camera

--- Assets/Scripts/DisableCameraAfterChunkFormation.cs
+++ Assets/Scripts/DisableCameraAfterChunkFormation.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class DisableCameraAfterChunkFormation : MonoBehaviour
 {
+    IEnumerator DisableCamea()
+    {
+        float t = 0;
+
+        while (t < 10)
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        PlayerCam.SetActive(true);
+        Destroy(gameObject);
+    }
+
+    [SerializeField] GameObject PlayerCam;
+
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 10);
+        StartCoroutine(DisableCamea());
     }
 
     // Update is called once per frame
