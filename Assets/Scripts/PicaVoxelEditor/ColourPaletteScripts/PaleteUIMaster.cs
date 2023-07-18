34e56ea09cf47a3b6a785a9ae989750fb1988680
Malhar Choure <chouremalhar@gmail.com>
2023-07-18 14:18:26 +0530

    colour palette disable enable changes

--- Assets/Scripts/PicaVoxelEditor/ColourPaletteScripts/PaleteUIMaster.cs
+++ Assets/Scripts/PicaVoxelEditor/ColourPaletteScripts/PaleteUIMaster.cs
@@ -7,7 +7,7 @@ public class PaleteUIMaster : MonoBehaviour
 {
     [SerializeField] private GameObject[] _ColorPaletes;
     [SerializeField] private int _Index=0;
-    //[SerializeField] private GameObject _ColourPicker;
+    [SerializeField] private GameObject _ColourPicker;
 
     public void Awake()
     {
@@ -16,16 +16,16 @@ public class PaleteUIMaster : MonoBehaviour
             _ColorPaletes[i].SetActive(false);
         }
         _ColorPaletes[_Index].SetActive(true);
-        //_ColourPicker.SetActive(false);
+        _ColourPicker.SetActive(false);
     }
     public void SwitchPanel(int index)
     { 
         _ColorPaletes[_Index].SetActive(false);
         _Index = index;
         _ColorPaletes[index].SetActive(true);
-        /*if(index==3)
+        if (index == 3)
         {
             _ColourPicker.SetActive(true);
-        }*/
+        }
     }
 }
