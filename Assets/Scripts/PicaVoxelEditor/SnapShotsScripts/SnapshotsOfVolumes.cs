d7d61c8565bd792e0b97e089f4770660e25f19c1
Malhar Choure <chouremalhar@gmail.com>
2023-07-20 11:54:03 +0530

    code cleanup

--- Assets/Scripts/PicaVoxelEditor/SnapShotsScripts/SnapshotsOfVolumes.cs
+++ Assets/Scripts/PicaVoxelEditor/SnapShotsScripts/SnapshotsOfVolumes.cs
@@ -16,12 +16,6 @@ public class SnapshotsOfVolumes : MonoBehaviour
         _Render[tempIndex] = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
         _Cam.targetTexture = _Render[tempIndex];
         _Snaps[tempIndex].texture = _Render[tempIndex];
-        /*//RenderTexture render= _Cam.targetTexture;
-        Texture2D camTexture = new Texture2D(render.width, render.height, TextureFormat.ARGB32,false);
-        RenderTexture.active = render;
-        _Material.mainTexture = render;
-        _Snaps[_Index].material =_Material;*/
     }
-    // Start is called before the first frame update
 
 }
