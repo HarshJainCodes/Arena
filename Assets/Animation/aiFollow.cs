8b3dad6cdef91cef52ccb223ad66c3874e8b0abd
Malhar Choure <chouremalhar@gmail.com>
2023-07-24 10:53:39 +0530

    Revert "Revert "oc culling""
    This reverts commit 70f1d3322ca041fc80b0b0ea94c4d2df08e61738.

--- Assets/Animation/aiFollow.cs
+++ Assets/Animation/aiFollow.cs
@@ -10,13 +10,15 @@ public class aiFollow : MonoBehaviour
 	// public Transform playerTransform;
 	private AIPath aiPath;
 	private Animator animator;
-	public float a; 
+	public float a;
+	public float SpeedMax=10;
 
 	// Start is called before the first frame update
 	void Start()
     {
         animator = GetComponent<Animator>();
 		aiPath = GetComponentInChildren<AIPath>();
+		aiPath.maxSpeed = SpeedMax;
     }
 
     // Update is called once per frame
