95d51d411bc276cdfd782a040e2206e994031a65
Malhar Choure <chouremalhar@gmail.com>
2023-08-03 16:30:38 +0530

    spider jump and spawn changes

--- Assets/Scripts/AI/Boss/SpawnMinions.cs
+++ Assets/Scripts/AI/Boss/SpawnMinions.cs
@@ -9,7 +9,7 @@ public class SpawnMinions : MonoBehaviour
     [SerializeField] private int _maxEnemies = 5;
     [SerializeField] private float timerLimit=2f;
     [SerializeField] private Animator bossAnimator;
-    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Transform[] _spawnPoint;
     [SerializeField] private Transform _player;
     [SerializeField] private int _force;
     [SerializeField] private Transform _spawns;
@@ -28,9 +28,10 @@ public class SpawnMinions : MonoBehaviour
         bool startSpawn = bossAnimator.GetBool("StartFiring");
         if(startSpawn && timer>timerLimit)
         {
-            currnetNo++;
+            
             timer = 0;
-            StartCoroutine(spawn());
+            StartCoroutine(spawn(currnetNo));
+            currnetNo++;
             if (currnetNo == _maxEnemies)
             {
                 bossAnimator.SetBool("StartFiring", false);
@@ -40,14 +41,14 @@ public class SpawnMinions : MonoBehaviour
         }
     }
 
-    IEnumerator spawn()
+    IEnumerator spawn(int no)
     {
         yield return new WaitForSeconds(1);
-        GameObject created = Instantiate(_minion, _spawnPoint.position, Quaternion.identity, _spawns);
+        GameObject created = Instantiate(_minion, _spawnPoint[no%_spawnPoint.Length].position, Quaternion.identity, _spawns);
         //created.transform.LookAt(_player);
         created.GetComponentInChildren<AIDestinationSetter>().target = _player;
-        Rigidbody rb = created.GetComponentInChildren<Rigidbody>();
-        rb.AddForce(created.transform.forward * _force);
+        //Rigidbody rb = created.GetComponentInChildren<Rigidbody>();
+        //rb.AddForce(created.transform.forward * _force);
         
     }
 }
---
+++ Assets/Scripts/AI/BossExplosiveSpawns/JumpTowardsPlayer.cs
@@ -0,0 +1,31 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTowardsPlayer : MonoBehaviour
+{
+    AIDestinationSetter _setter;
+    Transform _player;
+    [SerializeField]CharacterController _selfrb;
+    float _timer = 0f;
+    float _timerMax = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _setter=GetComponent<AIDestinationSetter>();
+        _player = _setter.target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+        if(Mathf.Abs(_player.position.y-transform.position.y)>3 && _timer>_timerMax)
+        {
+            _timerMax = Random.Range(2f,6f);
+            _timer = 0f;
+            _selfrb.SimpleMove((((_player.position - transform.position) / Vector3.Magnitude(_player.position - transform.position))+transform.up) * 500f);
+        }
+    }
+}
