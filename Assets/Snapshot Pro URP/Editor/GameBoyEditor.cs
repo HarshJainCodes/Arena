75b78a9c1de60144b88c68abde5830490f0fed6a
ChinmaySurve <92304154+ChinmaySurve@users.noreply.github.com>
2023-08-24 09:57:28 +0530

    Trying Different Shaders

---
+++ Assets/Snapshot Pro URP/Editor/GameBoyEditor.cs
@@ -0,0 +1,61 @@
+﻿namespace SnapshotShaders.URP
+{
+    using UnityEditor.Rendering;
+    using UnityEngine.Rendering.Universal;
+    using System.Reflection;
+    using UnityEditor;
+    using UnityEngine;
+
+    [VolumeComponentEditor(typeof(GameBoySettings))]
+    public class GameBoyEditor : VolumeComponentEditor
+    {
+        SerializedDataParameter enabled;
+        SerializedDataParameter darkestColor;
+        SerializedDataParameter darkColor;
+        SerializedDataParameter lightColor;
+        SerializedDataParameter lightestColor;
+        SerializedDataParameter powerRamp;
+
+        public override void OnEnable()
+        {
+            var o = new PropertyFetcher<GameBoySettings>(serializedObject);
+            enabled = Unpack(o.Find(x => x.enabled));
+            darkestColor = Unpack(o.Find(x => x.darkestColor));
+            darkColor = Unpack(o.Find(x => x.darkColor));
+            lightColor = Unpack(o.Find(x => x.lightColor));
+            lightestColor = Unpack(o.Find(x => x.lightestColor));
+            powerRamp = Unpack(o.Find(x => x.powerRamp));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (!SnapshotUtility.CheckEffectEnabled<GameBoy>())
+            {
+                EditorGUILayout.HelpBox("The Game Boy effect must be added to your renderer's Renderer Features list.", MessageType.Error);
+                if (GUILayout.Button("Add Game Boy Renderer Feature"))
+                {
+                    SnapshotUtility.AddEffectToPipelineAsset<GameBoy>();
+                }
+            }
+
+            PropertyField(enabled);
+            PropertyField(darkestColor);
+            PropertyField(darkColor);
+            PropertyField(lightColor);
+            PropertyField(lightestColor);
+            PropertyField(powerRamp);
+        }
+
+#if UNITY_2021_2_OR_NEWER
+        public override GUIContent GetDisplayTitle()
+        {
+            return new GUIContent("Game Boy");
+        }
+#else
+    public override string GetDisplayTitle()
+    {
+        return "Game Boy";
+    }
+#endif
+    }
+}
